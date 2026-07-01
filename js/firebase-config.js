ea119ea5092584ad02e33030e59da252180d15df
Andromedde1 <rybakovanton733@gmail.com>
2026-07-01 11:29:07 +0400

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,8 +1,9 @@
 window.FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyDL8_vL8cApKek4PFc1SRiY73SETwrDWow",
+  authDomain: "bankoffilms.firebaseapp.com",
+  projectId: "bankoffilms",
+  storageBucket: "bankoffilms.firebasestorage.app",
+  messagingSenderId: "834778395864",
+  appId: "1:834778395864:web:e1e58660e975f05e1193ad",
+  measurementId: "G-T90GWY6QRK"
 };
